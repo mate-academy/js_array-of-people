de25b9ae9054dc1305339b9dcaae76c2888ffa91
gestap <gestap@gmail.com>
2019-04-15 19:40:20 +0300

    modified solution

--- src/arrayOfPeople.js
+++ src/arrayOfPeople.js
@@ -40,7 +40,7 @@
 
 function arrayOfPeople(people) {
   let arr = [];
-  people.map((el) => {
+  people.filter((el) => {
     return arr.includes(el) ? arr.splice(arr.indexOf(el), 1) : arr.push(el);
   });
   return arr;
