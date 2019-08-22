1dfbe8282191cc142ccb3eaf5911c7e99fe7c36d
KseniiaMazan <ksusamazan@gmail.com>
2019-08-22 13:40:54 +0300

    simpler solution

--- src/arrayOfPeople.js
+++ src/arrayOfPeople.js
@@ -38,30 +38,17 @@
  * @return {number[]}
  */
 function arrayOfPeople(people) {
-  const set = new Set(people);
-  const peopleInRoom = [];
+  const setOfPeopleInRoom = new Set();
 
-  set.forEach(person => {
-    let counter = 0;
-
-    for (let pos = 0; pos < people.length;) {
-      const foundPos = people.indexOf(person, pos);
-
-      if (foundPos === -1) {
-        break;
-      }
-
-      pos = foundPos + 1;
-      counter++;
-    }
-
-    if (counter % 2 !== 0) {
-      peopleInRoom.push(person);
+  people.forEach(person => {
+    if (setOfPeopleInRoom.has(person)) {
+      setOfPeopleInRoom.delete(person);
+    } else {
+      setOfPeopleInRoom.add(person);
     }
   });
 
-  return peopleInRoom
-    .sort((a, b) => people.lastIndexOf(a) - people.lastIndexOf(b));
+  return [...setOfPeopleInRoom];
 }
 
 module.exports = arrayOfPeople;
