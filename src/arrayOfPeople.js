1e62fbfde533f73a25f7db66e0d694b8387910e9
VadimK <djvadya16@gmail.com>
2019-10-02 16:58:56 +0300

    done

--- src/arrayOfPeople.js
+++ src/arrayOfPeople.js
@@ -38,7 +38,17 @@
  * @return {number[]}
  */
 function arrayOfPeople(people) {
-  // write code here
+  const peopleInRoom = [];
+
+  people.forEach((person) => {
+    if (peopleInRoom.includes(person)) {
+      peopleInRoom.splice(peopleInRoom.indexOf(person), 1);
+    } else {
+      peopleInRoom.push(person);
+    }
+  });
+
+  return peopleInRoom;
 }
 
 module.exports = arrayOfPeople;
