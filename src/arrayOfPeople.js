307a387038b142e3850c64abd95693e0ba374a7c
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2019-11-20 14:27:58 +0200

    solution

--- src/arrayOfPeople.js
+++ src/arrayOfPeople.js
@@ -38,7 +38,15 @@
  * @return {number[]}
  */
 function arrayOfPeople(people) {
-  // write code here
+  const inside = [];
+  for (const person of people) {
+    if (inside.includes(person)) {
+      inside.splice(inside.indexOf(person), 1);
+    } else {
+      inside.push(person);
+    }
+  }
+  return inside;
 }
 
 module.exports = arrayOfPeople;
