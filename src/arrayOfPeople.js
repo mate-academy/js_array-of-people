4670cbdd76f98dea97588db9ac5e4be0fd207b1e
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-01-30 14:55:19 +0200

    arr of people

--- src/arrayOfPeople.js
+++ src/arrayOfPeople.js
@@ -38,7 +38,17 @@
  * @return {number[]}
  */
 function arrayOfPeople(people) {
-  // write code here
+  const peopleInTheRoom = [];
+
+  people.map((person) => {
+    if (peopleInTheRoom.includes(person)) {
+      peopleInTheRoom.splice(peopleInTheRoom.indexOf(person), 1);
+    } else {
+      peopleInTheRoom.push(person);
+    }
+  });
+
+  return peopleInTheRoom;
 }
 
 module.exports = arrayOfPeople;
