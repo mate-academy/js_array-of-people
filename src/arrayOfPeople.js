72f24730837035d152521873deca8be5225114f0
dima <tynindima@gmail.com>
2020-01-30 06:51:11 +0200

    Solution

--- src/arrayOfPeople.js
+++ src/arrayOfPeople.js
@@ -38,7 +38,17 @@
  * @return {number[]}
  */
 function arrayOfPeople(people) {
-  // write code here
+  const room = [];
+
+  people.forEach(person => {
+    if (!room.includes(person)) {
+      room.push(person);
+    } else {
+      room.splice(room.findIndex(index => index === person), 1);
+    }
+  });
+
+  return room;
 }
 
 module.exports = arrayOfPeople;
