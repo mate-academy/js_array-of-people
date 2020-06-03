d6f00a64a2d90da373a81abc135ed07b615bfca8
tyooma <flying.alien.godzilla@gmail.com>
2020-06-03 15:35:10 +0300

    Array of People solution

--- src/arrayOfPeople.js
+++ src/arrayOfPeople.js
@@ -38,7 +38,15 @@
  * @return {number[]}
  */
 function arrayOfPeople(people) {
-  // write code here
+  const peopleInRoom = [];
+
+  for (let i = 0; i < people.length; i++) {
+    peopleInRoom.includes(people[i])
+      ? peopleInRoom.splice(peopleInRoom.indexOf(people[i]), 1)
+      : peopleInRoom.push(people[i]);
+  }
+
+  return peopleInRoom;
 }
 
 module.exports = arrayOfPeople;
